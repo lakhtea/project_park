65b6a2128cf4f4b2a8800b4e26a0132eb88c8042
Lakhte Ali Agha <lagha@ltvco.com>
2022-07-14 20:08:25 -0400

    Create react app

---
+++ projectpark/src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="App">
+      <a
+        rel="noreferrer"
+        target="_blank"
+        href="https://github.com/lakhtea/project_park/blob/main/projectpark/src/projects/Calculator/index.html"
+      >
+        Calc
+      </a>
+    </div>
+  );
+}
+
+export default App;
